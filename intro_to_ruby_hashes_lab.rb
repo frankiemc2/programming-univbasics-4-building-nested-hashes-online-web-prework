2c8e83e32b15abf8f253b2f5a310c3c8ad752eb4
francisco cabrera <fjcabrera@buffalo.edu>
2019-10-09 23:11:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,11 +21,12 @@ def monopoly_with_third_tier
   monopoly =  {
     :railroads=>{
       :pieces=>4,
-      :rent_in_dollars=>{
-        :one_piece_owned
-        :two_pieces_owned
-        :three_pieces_owned
-        :four_pieces_owned
+      :rent_in_dollars=>
+      {
+        :one_piece_owned=50,
+        :two_pieces_owned=100,
+        :three_pieces_owned=150,
+        :four_pieces_owned=200,
       }
     }}
 end
